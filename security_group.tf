12c908e6541d9346414abfdddb6f374de1060bcc
code-commit-user+1-at-285058337712 <tomsebastian006@gmail.com>
2021-05-04 16:29:28 +0530

    working in alb

---
+++ security_group.tf
@@ -0,0 +1,22 @@
+resource "aws_security_group" "alb-sg" {
+  name        = "${var.stack}-alb-sg"
+  description = "ALB Security Group"
+  vpc_id      = aws_vpc.main.id
+
+  ingress {
+    protocol    = "tcp"
+    from_port   = 80
+    to_port     = 80
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port = 0
+    to_port   = 0
+    protocol  = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  tags = {
+    Name = "${var.stack}-alb-sg"
+  }
+}
